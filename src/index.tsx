0de362c4652a4267eff2550cc6e29fb416851ce6
HUNG-rushb <tdhtp2016@gmail.com>
2022-06-03 19:07:11 +0700

    ok

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1> Hii </h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
